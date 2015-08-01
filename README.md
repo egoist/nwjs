88576227721f871567949884912fa59d9646eb04
aprilorange <aprilorange.net@icloud.com>
2015-08-01 12:42:33 +0800

    test on win32

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 Inspired by [electron-prebuilt](https://github.com/mafintosh/electron-prebuilt)
 
+# install
+
+npm i -g nwjs --verbose
+
+# Usage
+
+```bash
+// log the version of nw.js you use to run your app
+nwjs info
+
+// install some version of nw.js
+nwjs 0.12.3 or nwjs 0.13.0-alpha2
+
+// run app
+nwjs
+nwjs ./app
+```
+
 ## License
 
 MIT.
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var fs = require('fs')
 var os = require('os')
+var path = require('path')
+var homePath = require('home-path')
+var spawn = require('child-process-promise').spawn
 var download = require('./lib/download')
-var version = '0.12.2'
+var config = require('./config')
+var version = config.useVersion
 
 // get platform due to nw.js's bad naming style
 var platform = os.platform()
@@ -15,10 +19,41 @@ if(platform.substring(0, 5) == 'linux') {
 platform = platform  + '-' + os.arch()
 
 module.exports = function(argv) {
-  console.log(argv)
   // usage: nwjs or nwjs . or nwjs C:\Users\sox\Dropbox\dev\app
-  var appPath = argv._[0] ? argv._[0] : '.'
-  console.log('Running nwjs prebuilts in '.cyan + appPath.cyan)
+  var opts = {
+    platform: platform,
+    version: version
+  }
 
-  download(platform, version)
+  switch (argv._[0]) {
+    case 'use':
+      opts.version = argv._[1] || version
+      require('./lib/install')(opts, config)
+      break
+    case 'info':
+      var info = 'Currently using nw.js ' + version + ' to run you apps'
+      console.log(info.green)
+      break
+    default:
+      prebuilt()
+  }
+
+  function prebuilt() {
+    var appPath = argv._[0] ? argv._[0] : '.'
+    console.log('Running nwjs prebuilts in '.cyan + appPath.cyan)
+
+    download(opts, mainAction)
+
+
+    function mainAction(err, files) {
+      if(err) return console.log(err)
+      var nwjs = path.join(homePath(), '.nwjs/' + version + '/nw.exe')
+      spawn(nwjs + ' ' + appPath)
+        .then(function() {
+          var info = 'Stopped running app ' + appPath + ' in nw.js ' + version
+          console.log(info.red)
+        })
+    }
+
+  }
 }
